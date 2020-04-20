9dad1fd174b687a17882d03def68b0cd690442ca
Patrick Olschewski <polschewski@yahoo.com>
2020-04-20 17:32:00 +0200

    first test of parser

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,27 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            JSONObject jsonSandwich = new JSONObject(json);
+            Sandwich sandwich = new Sandwich();
+
+            sandwich.setMainName(jsonSandwich.getString("name.mainName"));
+            sandwich.setImage(jsonSandwich.getString("image"));
+            //sandwich.setAlsoKnownAs();
+            sandwich.setPlaceOfOrigin(jsonSandwich.getString("placeOfOrigin"));
+            sandwich.setDescription(jsonSandwich.getString("description"));
+            //sandwich.setIngredients();
+
+            return sandwich;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
